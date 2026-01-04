f5c3ae85f9bdaaccdb2536b79fb664af7fcf31f9
HananFayyaz100 <hanan0371fiaz@gmail.com>
2026-01-04 20:43:16 +0500

    dotenv variable at top

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import projectRoutes2 from "./routes/projectRoutes2.js";
 
-dotenv.config();
 connectDB();
 
 const app = express();
