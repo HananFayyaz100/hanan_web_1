aedbcddc939b5187ec3d36f10d35302a9967451d
HananFayyaz100 <hanan0371fiaz@gmail.com>
2026-01-04 20:30:41 +0500

    add new route in index

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(express.json());
 
 app.use("/api/projects", projectRoutes);
 app.use("/api/projects2", projectRoutes2);
-
+app.get("work", (req, res) => {
+  res.send("Portfolio API is working")
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
